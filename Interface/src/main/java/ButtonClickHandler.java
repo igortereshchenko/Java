9e2634d00824a4ccfca7c5da0e8fc1a3bc5b2fc1
igortereshchenko <tereshchenko.igor@gmail.com>
2017-04-18 13:06:56 -0700

    Interface and Object added

---
+++ Interface/src/main/java/ButtonClickHandler.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Igor on 4/18/2017.
+ */
+public class ButtonClickHandler implements EventHandler {
+
+    public void execute() {
+        System.out.println("Кнопка нажата!");
+    }
+}
